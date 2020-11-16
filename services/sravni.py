02b5ae8430329c40bf230660c386e2ef59cba1bc
rzhvpw3457 <72699950+rzhvpw3457@users.noreply.github.com>
2020-11-17 00:27:47 +0300

    Add files via upload

---
+++ services/sravni.py
@@ -0,0 +1,13 @@
+from b0mb3r.services.service import Service
+
+
+class Sravni(Service):
+    async def run(self):
+        await self.get(
+            "https://www.sportmaster.ua/",
+            params={
+                "module": "users",
+                "action": "SendSMSReg",
+                "phone": self.formatted_phone,
+            },
+        )
